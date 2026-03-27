b23715fa0ec953e6a3c4c9140361e4b8c7fc7aa2
Arun Saini <shyamzacx@gmail.com>
2026-03-27 19:00:10 +0530

    armstong number

---
+++ Armstrong.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, originalNum, remainder, result = 0, n = 0, i, power;
+
+    printf("Enter an integer:");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        originalNum /= 10;
+        n++;
+    }
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+
+        power = 1;
+        for(i = 0; i < n; i++) {
+            power *= remainder;
+        }
+
+        result += power;
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
--- divisible.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-int main()
-{
-    int a;
-    printf("Enter  a number: ");
-    scanf("%",&a);
-    printf("%d",a%2 == 0);
-    return 0;
-}
