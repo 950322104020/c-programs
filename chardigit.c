bc1841efcfa52aad721c670630ad5a617f4e2220
Arun Saini <shyamzacx@gmail.com>
2026-03-27 19:28:38 +0530

    char is digit

---
+++ chardigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>   // VERY IMPORTANT
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (isdigit(ch))
+        printf("It is a digit\n");
+    else
+        printf("It is not a digit\n");
+
+    return 0;
+}
