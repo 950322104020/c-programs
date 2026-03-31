be5fc9bb889f6514e3cd87da2329e7da4b54a318
Arun Saini <shyamzacx@gmail.com>
2026-03-31 09:24:46 +0530

    greatest number

---
+++ Odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// to check odd or even number
+int main(){
+     int a;
+     printf("enter number:");
+     scanf("%d",&a);
+     if(a%2==0)
+     {
+      printf("number is even");
+
+     }
+     else{
+      printf("number is odd");
+     }
+     return 0;
+    }
+    
+   
